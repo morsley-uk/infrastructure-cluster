7d3b0f13df6e10e91a7faf604a0e51dbfb00b6a7
John Morsley <johnmorsley@gmail.com>
2020-02-29 10:47:51 +0000

    WiP

--- aws/install_nginx.sh
+++ aws/install_nginx.sh
@@ -6,7 +6,24 @@ echo '##########################################################################
 
 set -x
 
-apt-get update
+sudo apt-get update
+
+sudo wget http://nginx.org/keys/nginx_signing.key
+
+sudo apt-key add nginx_signing.key
+
+cd /etc/apt
+
+sudo chmod 777 sources.list
+
+sudo echo 'deb http://nginx.org/packages/ubuntu xenial nginx' >> sources.list
+sudo echo 'deb-src http://nginx.org/packages/ubuntu xenial nginx' >> sources.list
+
+sudo apt-get update
+
+sudo apt-get install nginx -y
+
+sudo service nginx start
 
 set +x
 
@@ -15,24 +32,3 @@ echo '# NGINX INSTALLED'
 echo '###############################################################################'
 
 return 0
-
-#sudo apt update
-
-#sudo apt install wget
-
-#sudo wget http://nginx.org/keys/nginx_signing.key
-
-#sudo apt-key add nginx_signing.key
-
-#cd /etc/apt
-
-#sudo chmod 777 sources.list
-
-#sudo echo 'deb http://nginx.org/packages/ubuntu xenial nginx' >> sources.list
-#sudo echo 'deb-src http://nginx.org/packages/ubuntu xenial nginx' >> sources.list
-
-#sudo apt-get update
-
-#sudo apt-get install nginx -y
-
-#sudo service nginx start
