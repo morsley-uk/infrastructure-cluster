d3805a887666d3a4989793124bd1014263a558dc
John Morsley <johnmorsley@gmail.com>
2020-03-06 19:16:30 +0000

    Switched on Terraform remote state

--- aws/terraform.tf
+++ aws/terraform.tf
@@ -6,12 +6,12 @@ terraform {
 
   required_version = ">= 0.12.0"
 
-  # backend "s3" {
-  #   bucket         = "morsley-uk-terraform-backend"
-  #   key            = "infrastructure"
-  #   region         = "eu-west-2"
-  #   dynamodb_table = "tfstatelock"
-  #   encypt         = true
-  # }
+  backend "s3" {
+    bucket         = "morsley-uk-terraform-backend"
+    key            = "infrastructure"
+    region         = "eu-west-2"
+    dynamodb_table = "tfstatelock-k8s"
+    encypt         = true
+  }
 
 }
