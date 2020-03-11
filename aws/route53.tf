d7c8821c6542a856c31ad0accbf99767d12addb8
John Morsley <johnmorsley@gmail.com>
2020-03-11 19:28:19 +0000

    Route 53

--- aws/route53.tf
+++ aws/route53.tf
@@ -5,17 +5,29 @@
 # rancher.morsley.io
 
 data "aws_route53_zone" "morsley-io" {
+  
   name         = var.domain_name
   private_zone = false
+  
 }
 
 resource "aws_route53_record" "rancher-a-record" {
+
   zone_id = data.aws_route53_zone.morsley-io.zone_id
   name    = var.hostname
   type    = "A"
   ttl     = 300
   records = [aws_instance.k8s.public_ip]
+
 }
+
+//resource "aws_route53_record" "rancher-cname-record" {
+//  zone_id = data.aws_route53_zone.morsley-io.zone_id
+//  name    = var.hostname
+//  type    = "CNAME"
+//  ttl     = 300
+//  records = [aws_instance.k8s.public_dns]
+//}
 
 output "route_53_name_servers" {
   value = data.aws_route53_zone.morsley-io.name_servers
