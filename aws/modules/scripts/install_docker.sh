f365d612177950a433f013e8c4da0ba5a54aaf59
John Morsley <johnmorsley@gmail.com>
2020-04-03 14:19:28 +0100

    Trying Rancher's install Docker script.

--- aws/modules/scripts/install_docker.sh
+++ aws/modules/scripts/install_docker.sh
@@ -14,13 +14,15 @@ echo '##########################################################################
 
 #set -x
 
-sudo apt-get update --quiet
+#sudo apt-get update --quiet
 
-sudo curl -fsSL https://releases.rancher.com/install-docker/19.03.5.sh -o get-docker.sh
+#sudo curl -fsSL https://releases.rancher.com/install-docker/19.03.5.sh -o get-docker.sh
 
-sudo chmod +x get-docker.sh
+#sudo chmod +x get-docker.sh
 
-sudo bash get-docker.sh
+#sudo bash get-docker.sh
+
+curl https://releases.rancher.com/install-docker/19.03.sh | sh
 
 sudo usermod -aG docker ubuntu
 
